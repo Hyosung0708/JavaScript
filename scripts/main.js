6faec6690d3d0f81b20f7cd9f37a05a442237492
Hyosung0708 <tlsgytjd0218@gmail.com>
2023-09-05 09:50:20 +0900

    test

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+let myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+
+// 변수 선언
+let myVariable
+// 값 할당
+myVariable = 'hello'
+
+console.log(myVariable)
+
+
+
+let myVariable2 = 'world'
+
+console.log(myVariable2)
+
+
+
+var a = 1
+let b = 2
+const c = 3
+
+console.log(a, b, c)
+
+a = 10
+b = 20
+c = 30
